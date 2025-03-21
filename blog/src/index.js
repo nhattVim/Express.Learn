f7bfbaf77db413695f68776743e6deab5c938b6c
nhattVim <nhattruong13112000@gmail.com>
2025-03-21 22:03:53 +0700

    update

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,25 +1,25 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
-const app = express();
-const port = 3000;
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
 
-const route = require("./routes");
+const route = require('./routes')
 
 // Configure Handlebars as template engine
-app.engine("hbs", engine({ extname: ".hbs" }));
-app.set("view engine", "hbs");
-app.set("views", "src/resources/views");
+app.engine('hbs', engine({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', 'src/resources/views')
 
 // Middleware
-app.use(express.static("public")); // static files
-app.use(morgan("combined")); // HTTP Logger
+app.use(express.static('public')) // static files
+app.use(morgan('combined')) // HTTP Logger
 
 // Route handler
 route(app)
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
+    console.log(`Server running on http://localhost:${port}`)
+})
